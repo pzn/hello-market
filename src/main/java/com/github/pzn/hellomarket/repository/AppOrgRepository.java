d977e8d9b2c6086cb247b46172e399cb2f60b4ba
Christophe Lé <pzn@users.noreply.github.com>
2018-03-07 19:43:12 -0500

    Fix Acegi and Homepage

--- src/main/java/com/github/pzn/hellomarket/repository/AppOrgRepository.java
+++ src/main/java/com/github/pzn/hellomarket/repository/AppOrgRepository.java
@@ -1,6 +1,7 @@
 package com.github.pzn.hellomarket.repository;
 
 import com.github.pzn.hellomarket.model.entity.AppOrg;
+import java.util.Set;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -15,10 +16,10 @@ public interface AppOrgRepository extends CrudRepository<AppOrg, Long> {
 
   AppOrg findByCode(String code);
 
-  @Query("SELECT ao FROM AppOrg ao JOIN FETCH ao.appUsers")
-  Iterable<AppOrg> findAllFetchUsers();
+  @Query("SELECT ao FROM AppOrg ao LEFT JOIN FETCH ao.appUsers")
+  Set<AppOrg> findAllFetchUsers();
 
-  @Query("SELECT ao FROM AppOrg ao JOIN FETCH ao.appUsers WHERE ao.code = :code")
+  @Query("SELECT ao FROM AppOrg ao LEFT JOIN FETCH ao.appUsers WHERE ao.code = :code")
   AppOrg findByCodeFetchUsers(@Param("code") String code);
 
   @Transactional
